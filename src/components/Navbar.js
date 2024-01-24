70ecbda253941dd2de0fe88012e9f6bb9c4107c5
Jannik Franz <jannik.franz@t-online.de>
2024-01-24 13:48:46 +0100

    get title and link in navbar from siteMetadata

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar({ location, title, menuLinks }) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container">
-            <Link className="navbar-brand" to="/">Briefe von und an Arnold Schönberg</Link>
+            <Link className="navbar-brand" to={menuLinks[0].link}>{ title }</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
