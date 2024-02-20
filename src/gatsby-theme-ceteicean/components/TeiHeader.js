2145a1fc3d84aaa52c56be728582b757239cd5a8
Jannik Franz <jannik.franz@t-online.de>
2024-02-20 10:50:45 +0100

    function for formatting dates from iso-format to german format

--- src/gatsby-theme-ceteicean/components/TeiHeader.js
+++ src/gatsby-theme-ceteicean/components/TeiHeader.js
@@ -5,6 +5,18 @@ import Alert from 'react-bootstrap/Alert'
 import "./teiHeader.css"
 
 const TeiHeader = ({teiNode}) => {
+
+  const formatDate = (date) => {
+    const dateArray = date.split("-")
+    switch (dateArray.length) {
+      case 1: return `${dateArray[0]}`
+      case 2: return `${dateArray[1]}.${dateArray[0]}`
+      case 3: return `${dateArray[2]}.${dateArray[1]}.${dateArray[0]}`
+      default:
+        console.log(`Error with date ${date}.`);
+    }
+  }
+
   const titleStmt = teiNode.getElementsByTagName("tei-titleStmt")[0]
   const title = titleStmt.getElementsByTagName("tei-title")[0].textContent
   const editor = titleStmt.getElementsByTagName("tei-editor")[0].textContent
@@ -12,7 +24,7 @@ const TeiHeader = ({teiNode}) => {
   const sender = senderInfo.getElementsByTagName("tei-persName")[0].textContent
   const place = senderInfo.getElementsByTagName("tei-placeName")[0]
   const date = senderInfo.getElementsByTagName("tei-date")[0]
-  const formatDate = date ? date.getAttribute("when") : "ohne Datum"
+  const checkDate = date ? formatDate(date.getAttribute("when")) : "ohne Datum"
   const addressee = teiNode.getElementsByTagName("tei-correspAction")[1].getElementsByTagName("tei-persName")[0]
 
   return (
@@ -23,7 +35,7 @@ const TeiHeader = ({teiNode}) => {
       <div className="metadata">
         <div className="row sender">
             <div className="col-md-3">Absender:</div>
-            <div className="col-md-9">{ sender }, { place ? `${place.textContent}, ` : ""}{ formatDate }</div>
+            <div className="col-md-9">{ sender }, { place ? `${place.textContent}, ` : ""}{ checkDate }</div>
         </div>
         <div className="row addressee">
             <div className="col-md-3">Empfänger:</div>
